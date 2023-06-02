ab94613d0735e9a58231f9ffc26ec194e6f8c02f
atolanrewaju2022 <135293313+atolanrewaju2022@users.noreply.github.com>
2023-06-02 08:58:54 -0500

    Update dev.tf

--- us-east-1/user-mgmt-app/environments/dev/dev.tf
+++ us-east-1/user-mgmt-app/environments/dev/dev.tf
@@ -1,5 +1,6 @@
 module "dev-ecs" {
- 
+   
+  source                    = "../.."
   vpc_cidr                  = "172.31.0.0/16"
   public_subnets_cidr       = [ "172.31.6.0/24" , "172.31.7.0/24" ]
   private_subnets_cidr      = [ "172.31.8.0/24" , "172.31.9.0/24" ]
